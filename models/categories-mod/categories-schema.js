df55e42055a7a28173c6b3726931604041b78025
Nsuliman <nsuliman88@gmail.com>
2020-01-31 21:54:21 +0200

    eslint errors fixed

--- models/categories-mod/categories-schema.js
+++ models/categories-mod/categories-schema.js
@@ -2,7 +2,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
-require('../products-mod/products-schema.js')
+require('../products-mod/products-schema.js');
 
 const categories = mongoose.Schema({
   name: { type: String, required: true },
@@ -10,24 +10,24 @@ const categories = mongoose.Schema({
 
 // collection/model name 
 categories.virtual('realPro', {
-    // collection/model name (categories)
-    ref : 'products',
-    // name inside categories schema 
-    localField : 'name',
-    foreignField : 'category',
-    justOne : false
+  // collection/model name (categories)
+  ref : 'products',
+  // name inside categories schema 
+  localField : 'name',
+  foreignField : 'category',
+  justOne : false,
 }); // end of mock categories 
 
 
 // do it before starting 
 categories.pre('findOne', function () {
-    try {
-        // the virtual name we have been created 
-        this.populate('realPro');
-    }
-    catch (err) {
-        console.error(err);
-    }
+  try {
+    // the virtual name we have been created 
+    this.populate('realPro');
+  }
+  catch (err) {
+    console.error(err);
+  }
 }); // end of population for categories 
 
 module.exports = mongoose.model('categories', categories);
--- routes/api.js
+++ routes/api.js
@@ -8,29 +8,29 @@ const proMod = require('../models/products-mod/products-model.js');
 
 // makes the products and categories route generic 
 function modelName(req,res,next){
-    // make the model name generic globally 
-    let model = req.params.model;
+  // make the model name generic globally 
+  let model = req.params.model;
 
-    // to modify the middleware request 
-    switch(model){
-        // api/v1/modelN === api/v1/categories 
-        case "catMod" : 
-        req.model = catMod;
-        next();
-        return;
+  // to modify the middleware request 
+  switch(model){
+  // api/v1/modelN === api/v1/categories 
+  case 'catMod' : 
+    req.model = catMod;
+    next();
+    return;
 
-        // api/v1/modelN === api/v1/produts 
-        case "proMod" : 
-        req.model = proMod;
-        next();
-        return;
+    // api/v1/modelN === api/v1/produts 
+  case 'proMod' : 
+    req.model = proMod;
+    next();
+    return;
 
-        // if the model name invalid , the middleware error will occars 
-        default: 
-        next(' This model Name Not Exist');
-        return;
+    // if the model name invalid , the middleware error will occars 
+  default: 
+    next(' This model Name Not Exist');
+    return;
 
-    } // end of switch statement 
+  } // end of switch statement 
 
 } // end of modelname function 
 
@@ -39,9 +39,9 @@ function modelName(req,res,next){
 router.param('model',modelName);
 
 // dynamic routes  
-router.get('api/v1/:model',getAll)
-router.get('api/v1/:model/:id',getOne)
-router.post('api/v1/:model',postOne)
+router.get('/api/v1/:model',getAll);
+router.get('/api/v1/:model/:id',getOne);
+router.post('/api/v1/:model',postOne);
 
 function getAll(req,res,next){
 
