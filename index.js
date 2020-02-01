a40ccf208a6c8567654f42f5c8e1342e68b2733e
Nsuliman <nsuliman88@gmail.com>
2020-02-01 15:47:29 +0200

    eslint more errors fixed

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const server = require('./lib/server.js');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+const catsch = require('./models/categories-mod/categories-schema.js');
+const prosch = require('./models/products-mod/products-schema.js');
+
+const MONGOOSE_URI='mongodb://localhost:27017/serverVir'
+
 dotenv.config();
 //const MONGOOSE_URI = 'mongodb://localhost:27017/class08';
 
-mongoose.connect(process.env.MONGOOSE_URI, { useNewUrlParser: true, useCreateIndex:true,useUnifiedTopology:true });
+mongoose.connect(MONGOOSE_URI, { useNewUrlParser: true, useCreateIndex:true,useUnifiedTopology:true });
 
 server.start();
--- routes/api.js
+++ routes/api.js
@@ -8,29 +8,29 @@ const proMod = require('../models/products-mod/products-model.js');
 
 // makes the products and categories route generic 
 function modelName(req, res, next) {
-    // make the model name generic globally 
-    let model = req.params.model;
+  // make the model name generic globally 
+  let model = req.params.model;
 
-    // to modify the middleware request 
-    switch (model) {
-        // api/v1/modelN === api/v1/categories 
-        case 'catMod':
-            req.model = catMod;
-            next();
-            return;
+  // to modify the middleware request 
+  switch (model) {
+  // api/v1/modelN === api/v1/categories 
+  case 'catMod':
+    req.model = catMod;
+    next();
+    return;
 
-        // api/v1/modelN === api/v1/produts 
-        case 'proMod':
-            req.model = proMod;
-            next();
-            return;
+    // api/v1/modelN === api/v1/produts 
+  case 'proMod':
+    req.model = proMod;
+    next();
+    return;
 
-        // if the model name invalid , the middleware error will occars 
-        default:
-            next(' This model Name Not Exist');
-            return;
+    // if the model name invalid , the middleware error will occars 
+  default:
+    next(' This model Name Not Exist');
+    return;
 
-    } // end of switch statement 
+  } // end of switch statement 
 
 } // end of modelname function 
 
@@ -48,51 +48,51 @@ router.post('/api/v1/:model',postOne);
 
 function getAll(req, res, next) {
 
-    req.model.get()
-        .then(data => {
-            let count = data.length;
-            res.status(200).json({ count, data });
-        })
-        .catch(next);
+  req.model.get()
+    .then(data => {
+      let count = data.length;
+      res.status(200).json({ count, data });
+    })
+    .catch(next);
 } // end of getAll function 
 
 function getOne(req, res, next) {
-    let id = req.param.id;
-    req.model.get(id)
-        .then(data => {
-            res.status(200).json(data);
-        })
-        .catch(next);
+  let id = req.param.id;
+  req.model.get(id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(next);
 } // end of getOne function
 
 function postOne(req, res, next) {
-    console.log('req.body : ', req.body);
-    req.model.post(req.body)
-        .then( data =>
-            {
-                res.status(201).json(data);
-            })
-            .catch(next);
+  console.log('req.body : ', req.body);
+  req.model.post(req.body)
+    .then( data =>
+    {
+      res.status(201).json(data);
+    })
+    .catch(next);
 } // end of postOne function
 
 function updateOne(req, res, next) {
-    let id = req.param.id;
-    req.model.update(id,req.body)
-        .then( data =>
-            {
-                res.status(200).json(data);
-            })
-            .catch(next);
+  let id = req.param.id;
+  req.model.update(id,req.body)
+    .then( data =>
+    {
+      res.status(200).json(data);
+    })
+    .catch(next);
 } // end of updateOne function
 
 function deleteOne(req, res, next) {
-    let id = req.param.id;
-    req.model.delete(id)
-        .then( data =>
-            {
-                res.status(200).json(data);
-            })
-            .catch(next);
+  let id = req.param.id;
+  req.model.delete(id)
+    .then( data =>
+    {
+      res.status(200).json(data);
+    })
+    .catch(next);
 } // end of deleteOne function
 
 module.exports = router;
